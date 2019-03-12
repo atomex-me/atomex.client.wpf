4bc2890b2d15f5767e13fc78ba214bd687527060
matsakiv <matsak.i.v@gmail.com>
2019-03-13 01:14:56 +0300

    add ci and tests

---
+++ Atomix.Client.Wpf.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Atomix.Client.Wpf.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
