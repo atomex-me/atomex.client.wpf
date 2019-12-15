d54efb289efbf52b8af8109fee8be80ae1f5c4f2
Igor Matsak <matsak.i.v@gmail.com>
2019-12-15 19:34:49 +0300

    update to v1.0.20

--- Atomex.Client.Wpf.Tests/Properties/AssemblyInfo.cs
+++ Atomex.Client.Wpf.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Atomex.Client.Wpf.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2018 Igor Matsak")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Atomex.Client.Wpf/Properties/AssemblyInfo.cs
+++ Atomex.Client.Wpf/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Atomex.Client.Wpf")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 Igor Matsak")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
