feaa490948bfb8d434b8c91087d40224e5670624
dmir <demonmir@inbox.ru>
2021-03-01 17:47:28 +0300

    TezosSendPage and Erc20SendPage

---
+++ Atomex.Client.Wpf/Views/SendViews/Erc20SendPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Atomex.Client.Wpf.Views.SendViews
+{
+    public partial class Erc20SendPage : Page
+    {
+        public Erc20SendPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Atomex.Client.Wpf/Views/SendViews/TezosSendPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Atomex.Client.Wpf.Views.SendViews
+{
+    public partial class TezosSendPage : Page
+    {
+        public TezosSendPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
